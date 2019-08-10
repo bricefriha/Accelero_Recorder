1401da2a51860662808a309c49c48678f158688d
Brice Friha <brice.friha@aol.fr>
2019-08-10 18:33:46 +0200

    Setting up MVVM

---
+++ AcceleroRecorder/AcceleroRecorder/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace GsbMobileXamarin.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        string title = string.Empty;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
