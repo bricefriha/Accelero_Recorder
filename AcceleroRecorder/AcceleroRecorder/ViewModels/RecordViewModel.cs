8ac8b7e47236022a655e51984df63078dd55302d
Brice Friha <brice.friha@aol.fr>
2019-08-15 18:20:48 +0200

    Show the in live data on the screen

--- AcceleroRecorder/AcceleroRecorder/ViewModels/RecordViewModel.cs
+++ AcceleroRecorder/AcceleroRecorder/ViewModels/RecordViewModel.cs
@@ -10,9 +10,44 @@ namespace AcceleroRecorder.ViewModels
     /// </summary>
     public class RecordViewModel : BaseViewModel
     {
+        private double xdata;
+        public double Xdata
+        {
+            get { return xdata; }
+            set
+            {
+                xdata = value;
+                OnPropertyChanged();
+            }
+        }
+        private double ydata;
+        public double Ydata
+        {
+            get { return ydata; }
+            set
+            {
+                ydata = value;
+                OnPropertyChanged();
+            }
+        }
+        private double zdata;
+        public double Zdata
+        {
+            get { return zdata; }
+            set
+            {
+                zdata = value;
+                OnPropertyChanged();
+            }
+        }
         public RecordViewModel()
         {
             this.Title = "Recording";
+
+            // Set accelerometers data up
+            this.Xdata = 0;
+            this.Ydata = 0;
+            this.Zdata = 0;
         }
     }
 }
--- AcceleroRecorder/AcceleroRecorder/Views/RecordPage.xaml.cs
+++ AcceleroRecorder/AcceleroRecorder/Views/RecordPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,17 +13,75 @@ namespace AcceleroRecorder.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RecordPage : ContentPage
     {
+        
+        RecordViewModel vm ;
+        // Set speed delay for monitoring changes.
+        SensorSpeed speed = SensorSpeed.UI;
         public RecordPage()
         {
             InitializeComponent();
 
+            // Bind the viewmodel with the view
             BindingContext = new RecordViewModel();
+
+            // Get the viewmodel
+            vm = (RecordViewModel)BindingContext;
+
+            // Register for reading changes, be sure to unsubscribe when finished
+            Accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+        }
+        void Accelerometer_ReadingChanged(object sender, AccelerometerChangedEventArgs e)
+        {
+            var data = e.Reading;
+
+            // Set X data
+            vm.Xdata = Convert.ToDouble(data.Acceleration.X);
+
+            // Set Y data
+            vm.Ydata = Convert.ToDouble(data.Acceleration.Y);
+
+            // Set Z data
+            vm.Zdata = Convert.ToDouble(data.Acceleration.Z);
         }
 
         private void BtnRecord_Clicked(object sender, EventArgs e)
         {
             // Change the style of the button
             SwitchOnOrOff((Button)sender);
+
+            // Start or Stop the accelerometer
+            ToggleAccelerometer();
+
+        }
+        /// <summary>
+        /// Method allowing to Start or Stop the accelerometer
+        /// </summary>
+        public void ToggleAccelerometer()
+        {
+            try
+            {
+                if (Accelerometer.IsMonitoring)
+                {
+                    // Stop the Accelerometer
+                    Accelerometer.Stop();
+
+                    // Clean data on the screen
+                    vm.Xdata = 0;
+                    vm.Ydata = 0;
+                    vm.Zdata = 0;
+                }
+                else
+                    // Start the Accelerometer
+                    Accelerometer.Start(speed);
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Feature not supported on device
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
         }
 
         /// <summary>
