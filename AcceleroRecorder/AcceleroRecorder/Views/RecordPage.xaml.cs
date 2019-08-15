b6b102f95eb213e02274785e6f6d421bba5fa30a
Brice Friha <brice.friha@aol.fr>
2019-08-15 17:14:24 +0200

    I made the UI

--- AcceleroRecorder/AcceleroRecorder/Views/RecordPage.xaml.cs
+++ AcceleroRecorder/AcceleroRecorder/Views/RecordPage.xaml.cs
@@ -19,5 +19,41 @@ namespace AcceleroRecorder.Views
 
             BindingContext = new RecordViewModel();
         }
+
+        private void BtnRecord_Clicked(object sender, EventArgs e)
+        {
+            // Change the style of the button
+            SwitchOnOrOff((Button)sender);
+        }
+
+        /// <summary>
+        /// Switch a button On or Off
+        /// </summary>
+        /// <param name="sender">the button</param>
+        private void SwitchOnOrOff(Button btn)
+        {
+            try
+            {
+                // Verified the state of the button
+                switch (btn.CornerRadius)
+                {
+                    // If it is Off
+                    case 100:
+                        //Set it On
+                        btn.CornerRadius = 10;
+                        break;
+
+                    // If it is On
+                    case 10:
+                        //Set it Off
+                        btn.CornerRadius = 100;
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
