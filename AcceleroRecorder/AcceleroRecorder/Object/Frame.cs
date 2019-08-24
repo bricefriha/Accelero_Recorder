997b06b1b69de12e4f5fab97cf87ef4ba872b256
Brice Friha <brice.friha@aol.fr>
2019-08-24 18:03:05 +0200

    Create Frame object

---
+++ AcceleroRecorder/AcceleroRecorder/Object/Frame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcceleroRecorder.Object
+{
+    public class Frame
+    {
+        // Declare attribut
+        private DateTime time;
+        private float xData;
+        private float yData;
+        private float zData;
+
+        /// <summary>
+        /// Constructor of the Frame object
+        /// </summary>
+        /// <param name="time"> Time of the frame </param>
+        /// <param name="xData"> x data of the accelerometer a this time </param>
+        /// <param name="yData"> y data of the accelerometer a this time</param>
+        /// <param name="zData"> z data of the accelerometer a this time</param>
+        public Frame(DateTime time, float xData, float yData, float zData)
+        {
+            this.time = time;
+            this.xData = xData;
+            this.yData = yData;
+            this.zData = zData;
+        }
+
+        // Encapse
+        public DateTime Time { get => time;/* set => time = value;*/ }
+        public float XData { get => xData; /*set => xData = value;*/ }
+        public float YData { get => yData; /*set => yData = value;*/ }
+        public float ZData { get => zData; /*set => zData = value;*/ }
+    }
+}
---
+++ AcceleroRecorder/AcceleroRecorder/Object/Record.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcceleroRecorder.Object
+{
+    /// <summary>
+    /// Class of the object which contain all the recording attributs
+    /// </summary>
+    public class Record
+    {
+
+    }
+}
