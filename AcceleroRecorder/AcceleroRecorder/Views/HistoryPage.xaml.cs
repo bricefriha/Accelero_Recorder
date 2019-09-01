110dbff2c3f52f5c7d7b2510acb7f8a487ac6d0e
Brice Friha <brice.friha@aol.fr>
2019-09-01 17:54:03 +0200

    Make the button Delete all on the history page + Displayalert

--- AcceleroRecorder/AcceleroRecorder/Views/HistoryPage.xaml.cs
+++ AcceleroRecorder/AcceleroRecorder/Views/HistoryPage.xaml.cs
@@ -40,26 +40,50 @@ namespace AcceleroRecorder.Views
             Navigation.PushAsync(new DetailRecordPage(filename));
         }
 
-        private void BtnDelete_Clicked(object sender, EventArgs e)
+        private async void BtnDelete_Clicked(object sender, EventArgs e)
         {
             // Get the list view item index
             var id = (int)((Button)sender).CommandParameter;
 
-            // Delete the record
-            DeleteRecord(id);
+            // Get the record
+            RecordItem rec = ((HistoryViewModel)BindingContext).Records[id];
 
+            // Display a confirm pop up
+            if (await DisplayAlert("Delete " + rec.Title, "Are you sure to delete this record? \n(No move back possible)", "Yes, I am sure", "Cancel"))
+            {
+                // Delete the record
+                DeleteRecord(rec);
+            }
         }
 
-        private void DeleteRecord(int id)
+        /// <summary>
+        ///  Method which delete a record and reload the page
+        /// </summary>
+        /// <param name="recordItem"> record we need to delete </param>
+        private void DeleteRecord(RecordItem recordItem)
         {
             // Get the filename of the selected record
-            string filename = ((HistoryViewModel)BindingContext).Records[id].Filename;
+            string filename = recordItem.Filename;
 
             // Remove the record
             File.Delete(filename);
 
             // Reload the Binding Context
             BindingContext = new HistoryViewModel();
+        }
+
+        private async void BtnDeleteAll_Clicked(object sender, EventArgs e)
+        {
+            // Display a confirm pop up
+            if (await DisplayAlert("Delete all", "Are you sure to delete all the records? \n(No move back possible)", "Yes, I am sure", "Cancel"))
+            {
+                // Remove every single record
+                foreach (RecordItem recordItem in ((HistoryViewModel)BindingContext).Records)
+                {
+                    DeleteRecord(recordItem);
+                }
+            };
+            
         }
     }
 }
